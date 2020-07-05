970cf05527933bfb6daaf1468808076bb7dc69ec
Terry Chia <terrycwk1994@gmail.com>
2020-07-05 16:10:44 +0800

    Add option for writing output as JSON.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use clap::{App, AppSettings, Arg};
+use serde::{Deserialize, Serialize};
 use std::collections::HashSet;
 use std::fs::File;
 use std::io::BufReader;
@@ -18,6 +19,14 @@ fn main() -> Result<()> {
                 .help("APK file to analyze.")
                 .takes_value(true),
         )
+        .arg(
+            Arg::with_name("OUTPUT")
+                .short("o")
+                .long("output")
+                .value_name("FILE")
+                .help("Write results to a JSON file.")
+                .takes_value(true),
+        )
         .get_matches();
 
     let apk = matches.value_of("APK").unwrap();
@@ -30,8 +39,13 @@ fn main() -> Result<()> {
     let files: HashSet<String> = zip.file_names().map(|x| x.to_owned()).collect();
     let results = run_checks(files)?;
 
-    for r in results {
+    for r in &results {
         r.print_results();
+    }
+
+    if let Some(o) = matches.value_of("OUTPUT") {
+        let file = File::create(o)?;
+        serde_json::to_writer(file, &results)?;
     }
 
     Ok(())
@@ -55,13 +69,14 @@ fn run_checks(files: HashSet<String>) -> Result<Vec<CheckResults>> {
     Ok(results)
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct CheckResults {
     pub name: String,
     pub matches: Vec<String>,
 }
 
 impl CheckResults {
-    fn print_results(self) {
+    fn print_results(&self) {
         println!("========================================");
         println!("[+] {} detected", self.name);
 
