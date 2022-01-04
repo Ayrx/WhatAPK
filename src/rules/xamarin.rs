da14f341e1e3fe90f8827f7afbf7d288dddd5c06
Terry Chia <terry@ayrx.me>
2022-01-04 22:22:59 +0800

    Run `cargo fmt`.

--- src/rules/xamarin.rs
+++ src/rules/xamarin.rs
@@ -2,11 +2,7 @@ use regex::RegexSet;
 use std::collections::HashSet;
 
 pub fn check(f: &HashSet<String>) -> Option<crate::CheckResults> {
-    let re = RegexSet::new(&[
-        r"lib/.*/libxamarin-app.so",
-        r"lib/.*/libmono-native.so",
-    ])
-    .unwrap();
+    let re = RegexSet::new(&[r"lib/.*/libxamarin-app.so", r"lib/.*/libmono-native.so"]).unwrap();
 
     let mut matches: Vec<String> = f.iter().filter(|path| re.is_match(path)).cloned().collect();
     matches.sort();
